fba96117cc69c616749e452990d4abbb8a4808e9
Márcio de Freitas Viana <83033976+VianaMarcioDev@users.noreply.github.com>
2022-09-04 20:28:30 -0300

    Utilizando estruturas condicionais

--- src/main.kt
+++ src/main.kt
@@ -10,5 +10,20 @@ fun main(args: Array<String>) {
     println("Número da Conta $numeroConta")
     println("Saldo da conta $saldo")
 
+//Utilizando if
+    if (saldo > 0.0){
+        println("Conta é positiva")
+    }else if (saldo == 0.0){
+        println("Conta é neutra")
+    }else{
+        println("Conta é negativa")
+    }
+
+//Utilizando when
+    when {
+        saldo > 0.0 -> println("Conta é positiva")
+        saldo == 0.0 -> println("Conta é neutra")
+        else -> println("Conta é negativa")
+    }
 
 }
