d9b50fc4399d4efacf1459d59a68073c6a0d19d2
Márcio de Freitas Viana <83033976+VianaMarcioDev@users.noreply.github.com>
2022-09-04 20:45:19 -0300

    utilizando estruturas de repetição

--- src/main.kt
+++ src/main.kt
@@ -1,21 +1,60 @@
 fun main(args: Array<String>) {
     println("Bem-vindo ao KotlinBank")
-    val titular : String = "Márcio"
-    val numeroConta: Int = 1000
-    var saldo: Double = 0.0
-    saldo = 100 + 2.0
-    saldo += 200
+    println()
 
-    println("Titular $titular")
-    println("Número da Conta $numeroConta")
-    println("Saldo da conta $saldo")
+    //i in 1..5 = range
+    //i in 5..1 = downTo
+    //step = de quantos em quantos faz a interação
+    //break = sai do laço quando encontrar aquela condição
+    //continue = pula quando encontrar aquela condição mas continua laço
+    for (i in 5 downTo 1 step 2) {
 
-//Utilizando if
-    if (saldo > 0.0){
+        if (i == 4) {
+            break
+        }
+        val titular: String = "Márcio $i"
+        val numeroConta: Int = 1000 + i
+        var saldo: Double = i + 10.0
+
+
+        println("Titular $titular")
+        println("Número da Conta $numeroConta")
+        println("Saldo da conta $saldo")
+        println()
+
+// Chamar função que está fora do método main
+//  testaCondicoes(saldo)
+
+
+        println(i)
+    }
+
+    //utilizando while para fazer iterações
+    var i = 1
+    while (i <= 5) {
+        val titular: String = "Márcio $i"
+        val numeroConta: Int = 1000 + i
+        var saldo: Double = i + 10.0
+
+
+
+        println("Titular $titular")
+        println("Número da Conta $numeroConta")
+        println("Saldo da conta $saldo")
+        println()
+        i++
+    }
+
+
+}
+
+fun testaCondicoes(saldo: Double) {
+    //Utilizando if
+    if (saldo > 0.0) {
         println("Conta é positiva")
-    }else if (saldo == 0.0){
+    } else if (saldo == 0.0) {
         println("Conta é neutra")
-    }else{
+    } else {
         println("Conta é negativa")
     }
 
@@ -25,5 +64,4 @@ fun main(args: Array<String>) {
         saldo == 0.0 -> println("Conta é neutra")
         else -> println("Conta é negativa")
     }
-
 }
