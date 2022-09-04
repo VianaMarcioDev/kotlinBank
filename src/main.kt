38eee7d2c5fb433ac33bac0e3dc173622288c724
Márcio de Freitas Viana <83033976+VianaMarcioDev@users.noreply.github.com>
2022-09-04 20:20:56 -0300

    Aprendendo Kotlin com procedural e mudança progressiva para Orientação a Objetos

---
+++ src/main.kt
@@ -0,0 +1,14 @@
+fun main(args: Array<String>) {
+    println("Bem-vindo ao KotlinBank")
+    val titular : String = "Márcio"
+    val numeroConta: Int = 1000
+    var saldo: Double = 0.0
+    saldo = 100 + 2.0
+    saldo += 200
+
+    println("Titular $titular")
+    println("Número da Conta $numeroConta")
+    println("Saldo da conta $saldo")
+
+
+}
